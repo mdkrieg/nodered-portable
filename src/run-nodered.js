d519978245fe621137d455ee64951ee682cacc53
Matthew Krieg <mdkrieg@mtu.edu>
2021-11-05 21:11:58 -0700

    v1.1.0

---
+++ src/run-nodered.js
@@ -0,0 +1,66 @@
+module.exports = function(){
+    const http = require('http');
+    const express = require("express");
+
+    const WebSocket = require('ws');
+    const WSv = new WebSocket.Server({port: 50820});
+    console.log("foo")
+    let socket;
+    let inputs;
+    WSv.on('connection', function(s) {
+        socket = s;
+        // When you receive a message, send that message to every socket.
+        socket.on('message', function(msg) {
+            inputs = JSON.parse(msg);
+        });
+
+        // Only start NR on websocket close
+        socket.on('close', function() {
+            startNodeRED();
+        });
+    });
+
+    function startNodeRED(){
+        var RED = require("../node_modules/node-red");
+
+        // Create an Express app
+        var app = express();
+        
+        // Create a server
+        var REDserver = http.createServer(app);
+
+        // Create the settings object - see default settings.js file for other options
+        var settings = {
+            httpAdminRoot: inputs.adminPath || "/red",
+            httpNodeRoot: inputs.nodePath || "/api",
+            userDir:"home",
+            httpStatic:"public",
+            contextStorage: {
+                default: {
+                    module:"localfilesystem",
+                    config: {
+                        dir: "context"
+                    }
+                }
+            }
+        };
+
+        // Initialise the runtime with a server and settings
+        RED.init(REDserver,settings);
+        
+        // Add a simple route for static content served from 'public'
+        app.use("/",express.static(settings.httpStatic));
+        // Serve the editor UI from /red
+        app.use(settings.httpAdminRoot,RED.httpAdmin);
+        // Serve the http nodes UI from /api
+        app.use(settings.httpNodeRoot,RED.httpNode);
+        
+        REDserver.listen(inputs.port || 1880);
+        
+        // Start the runtime
+        RED.start();
+    }
+};
+// if (require.main === module) {
+//     module.exports();
+// }
